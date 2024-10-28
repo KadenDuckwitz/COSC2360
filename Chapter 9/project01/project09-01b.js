ab128e15afffc1fac4cbb9954824afbb98a51dcf
KadenDuckwitz <kadenduckwitz@westernwyoming.edu>
2024-10-27 19:28:16 -0600

    Module 11 Hands-On Project #1

---
+++ Chapter 9/project01/project09-01b.js
@@ -0,0 +1,24 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Project 09-01
+
+      Project to read field values from a query string
+      Author: Kaden Duckwitz
+      Date: 10/27/24
+
+      Filename: project09-01b.js
+*/
+
+    let query = location.search.slice(1);
+    query = query.replace(/\+/g, " ");
+    query = decodeURIComponent(query);
+
+    let cardFields = query.split(/&/g);
+
+    for (let item of cardFields) {
+        let nameValue = item.split(/=/);
+        let name = nameValue[0];
+        let value = nameValue[1];
+        document.getElementById(name).textContent = value;
+    }
